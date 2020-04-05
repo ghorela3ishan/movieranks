d90d5f4450841bd4f3db94be47d303ecba2d5cdb
priyamvada.pant <ghorela.ishan@gmail.com>
2020-04-05 22:31:08 +0530

    changed tuple layout

--- src/components/Feed/ListTuple/ListTuple.js
+++ src/components/Feed/ListTuple/ListTuple.js
@@ -2,10 +2,12 @@ import React from "react";
 
 export default class ListTuple extends React.Component {
     render() {
-        let { name } = this.props.item;
+        let { name, votes, isVoted } = this.props.item;
         return (
-            <div>
-                {name}
+            <div style={{border: '1px solid red', margin: '10px'}}>
+                Film name: {name} <br/>
+                Total votes: {votes} <br/>
+                {!isVoted && "+"}
             </div>
         )
     }
