7788b802ab62f2d4087d3427eddadf78c7c24094
priyamvada.pant <ghorela.ishan@gmail.com>
2020-04-09 15:23:55 +0530

    added upvote animation

--- src/components/Feed/ListTuple/ListTuple.js
+++ src/components/Feed/ListTuple/ListTuple.js
@@ -1,21 +1,36 @@
 import React from "react";
+import Spinner from 'react-bootstrap/Spinner';
 import "./ListTuple.scss";
 
 export default class ListTuple extends React.Component {
     state = {
-        upVote: true
+        upVote: true,
+        showLoader: false
+    }
+
+    onUpvoteClick = (_id) => {
+        this.props.handleUpvoteClick(_id);
+        this.setState({
+            showLoader: true
+        })
     }
 
     render() {
         let { _id, name, votes, isUpVoted } = this.props.item;
         let { handleUpvoteClick, rank } = this.props;
+        let { showLoader } = this.state;
+
         return (
             <div className='tupleCont'>
                 <div className='nameSec'>
                     <div className='nameCont'>{name}</div>
                     {
-                        isUpVoted ? <i className='upVotedIcon'>&#9829;</i> :  
-                        <input type="button" value="Upvote" className='upVoteBtn' onClick={() => handleUpvoteClick(_id)} />
+                        isUpVoted ? <i className='upVotedIcon'>&#9829;</i> :
+                        ( showLoader ? 
+                            <Spinner animation="grow" variant="danger" />  
+                            :
+                            <input type="button" value="Upvote" className='upVoteBtn' onClick={() => this.onUpvoteClick(_id)} />
+                        )  
                         
                     }
                 </div>
