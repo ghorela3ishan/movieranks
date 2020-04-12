dcf44d711f1b65db421d2db0330e5d7add9b5a48
priyamvada.pant <ghorela.ishan@gmail.com>
2020-04-12 14:20:01 +0530

    removed url

--- src/components/Feed/Footer/Footer.js
+++ src/components/Feed/Footer/Footer.js
@@ -8,7 +8,7 @@ export default class Footer extends React.Component {
                     <div>&copy; 2020 MovieRanks</div>
                     <span>Team: </span>
                         <a className='profLink' target="_blank" href="https://www.linkedin.com/in/ghorela3ishan/">Ishan Ghorela</a>
-                        <a className='profLink' target="_blank" href="https://www.linkedin.com/in/chirag-a-792936133/">Chirag Arora</a>
+                        <a className='profLink' target="_blank" href="">Chirag Arora</a>
             </div>
         )
     }
